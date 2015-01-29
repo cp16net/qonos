6b0c2b4e7e9bbac663dbc4b5440ecdc14dde5213
Craig Vyvial <cp16net@gmail.com>
2015-01-28 23:17:12 -0600

    adding some changes to easily test out qonos for cdb mysql

--- run_dev_scheduler.sh
+++ run_dev_scheduler.sh
@@ -2,8 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR="$DIR/etc/qonos"
-CONFIG_FILE="$CONFIG_DIR/qonos-api.conf"
-ROOT_CONFIG="/etc/qonos/qonos-api.conf"
+CONFIG_FILE="$CONFIG_DIR/qonos-scheduler.conf"
+ROOT_CONFIG="/etc/qonos/qonos-scheduler.conf"
 if [[ -f $ROOT_CONFIG ]]; then
     CONFIG_FILE=$ROOT_CONFIG
 fi
@@ -14,4 +14,4 @@ then
     cp $CONFIG_FILE.sample $CONFIG_FILE
 fi
 
-$DIR/bin/qonos-api --config-file=$CONFIG_FILE
+$DIR/bin/qonos-scheduler --config-file=$CONFIG_FILE
--- run_dev_worker.sh
+++ run_dev_worker.sh
@@ -2,8 +2,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CONFIG_DIR="$DIR/etc/qonos"
-CONFIG_FILE="$CONFIG_DIR/qonos-api.conf"
-ROOT_CONFIG="/etc/qonos/qonos-api.conf"
+CONFIG_FILE="$CONFIG_DIR/qonos-worker.conf"
+ROOT_CONFIG="/etc/qonos/qonos-worker.conf"
 if [[ -f $ROOT_CONFIG ]]; then
     CONFIG_FILE=$ROOT_CONFIG
 fi
@@ -14,4 +14,4 @@ then
     cp $CONFIG_FILE.sample $CONFIG_FILE
 fi
 
-$DIR/bin/qonos-api --config-file=$CONFIG_FILE
+$DIR/bin/qonos-worker --config-file=$CONFIG_FILE
